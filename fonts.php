63877ee8ff388f733c9394a4d019dde6a80ea56a
phillipwright2 <bootsie8917@gmail.com>
2014-11-11 23:25:35 -0800

    Create fonts.php

---
+++ fonts.php
@@ -0,0 +1,7 @@
+http://www.quora.com/JavaScript-programming-language?share=  $font_file = 'font.ttf' ;
+  $font_size = 56 ;
+  $text_color = '#ffffff' ;
+  $background_color = '#000000' ;
+  $transparent = true ;
+  $cache_images = true ;
+  $cache_folder = 'cache' ;
